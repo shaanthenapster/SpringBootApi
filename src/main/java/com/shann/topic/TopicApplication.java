7f6f5013bd07f44f110566731edb81909ada26d3
shaan <shaanthenapster@gmail.com>
2018-05-21 17:02:48 +0530

    JPA dependency added

--- src/main/java/com/shann/topic/TopicApplication.java
+++ src/main/java/com/shann/topic/TopicApplication.java
@@ -1,5 +1,5 @@
 package com.shann.topic;
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
